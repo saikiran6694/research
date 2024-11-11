881054ec3e57033531aadb7dacb8d2d022dd03d5
Saikiran <70024114+saikiran6694@users.noreply.github.com>
2024-11-11 20:18:56 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} transparent`}>{children}</body>
+      <body className={`${inter.className} bg-transparent`}>{children}</body>
     </html>
   );
 }
